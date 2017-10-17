366841df7161a119d4a42071be34f00155dbea22
Nick Barber <nickwbarber@gmail.com>
2017-10-17 09:47:06 -0500

    Remove unnecessary getters and setters

--- intervaltree/__init__.py
+++ intervaltree/__init__.py
@@ -3,60 +3,28 @@ class IntervalTree:
                  data_list,
                  get_low_function,
                  get_high_function):
-        self._sorted_data = sorted(
+        self.sorted_data = sorted(
             data_list,
             key=lambda x: get_low_function(x)
         )
-        self._root_index = len(self._sorted_data)//2
+        self.root_index = len(self.sorted_data)//2
 
-        self._root = Node(
+        self.root = Node(
             self._sorted_data[self._root_index],
             lambda x: get_low_function(x),
             lambda x: get_high_function(x),
         )
-
-    def get_root(self):
-        return self._root
 
 class Node:
     def __init__(self,
                  data,
                  get_low_function,
                  get_high_function):
-        self._data = data
-        self._get_low_function = get_low_function
-        self._get_high_function = get_high_function
-        self._low = self._get_low_function(self._data)
-        self._high = self._get_high_function(self._data)
-        self._left = None
-        self._right = None
-        self._max = self._high
-
-    def get_data(self):
-        return self._data
-
-    def get_low(self):
-        return self._get_low_function(self.get_data())
-
-    def get_high(self):
-        return self._get_high_function(self.get_data())
-
-    def get_left(self):
-        return self._left
-
-    def set_left(self,
-                 data):
-        self._left = data
-
-    def get_right(self):
-        return self._right
-
-    def set_right(self,
-                  data):
-        self._right = data
-
-    def get_max(self):
-        return self._max
-
-    def set_max(self):
-        return self._max
+        self.data = data
+        self.get_low_function = get_low_function
+        self.get_high_function = get_high_function
+        self.low = self._get_low_function(self._data)
+        self.high = self._get_high_function(self._data)
+        self.max = self._high
+        self.left = None
+        self.right = None
